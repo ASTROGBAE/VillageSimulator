a3d1653df1cdc3325886f1a469610e5a00e15813
ASTROGBAE <gabebrett@iinet.net.au>
2022-01-13 14:04:48 +1100

    made main method call village object

--- village/main.py
+++ village/main.py
@@ -12,11 +12,12 @@ from datetime import date, timedelta
 # village event iterator
 startDate = date.fromisoformat('0853-01-01')
 events = [Population(startDate)] # TODO refactor starting date to make it better?
-duration = timedelta(days=10)
+duration = timedelta(weeks=52*20)
+town = village.Village("Appleton", startDate)
 
-for curSecond in range(0, int(duration.total_seconds())): # calculate by second, TODO work out how to deal with returning total seconds not equal?
+for i in range(0, town.dateDiffToIt(duration)): # calculate by second, TODO work out how to deal with returning total seconds not equal?
     for event in events: 
-        ticket = event.work((startDate + timedelta(seconds=curSecond))) # think and log time 
-        village.stats[ticket[0]] += ticket[1] # update value (key, value)
+        ticket = event.work((startDate + town.getItTimeDelta(i))) # think and log time 
+        town.stats[ticket[0]] += ticket[1] # update value (key, value)
     
 print("The end!")
