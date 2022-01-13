065df1db2520794962f73d6306d7bbbd241b2131
ASTROGBAE <gabebrett@iinet.net.au>
2022-01-13 13:58:24 +1100

    changed village from method bundle to class

--- village/village.py
+++ village/village.py
@@ -1,23 +1,25 @@
-# singleton Village object
+from datetime import timedelta
+from temporal import Temporal
 
-stats = {}
-stats["name"] = "appleton"
-stats["day"] = 0
-stats["food"] = 10
-stats["population"] = 5
-stats["plots"] = {"farm":2, "house":1}
+class Village(Temporal):
     
-# get village statistic
-def getStat(statistic):
-    return stats[statistic]
-
-# get village plot statistic
-def getPlot(plot):
-    return stats["plots"][plot]
-
-# set village statistic
-def setStat(statistic, value):
-    stats[statistic] = value
+    def __init__(self, name, date):
+        self.stats = {"name": name,
+        "day": 0, "food": 10, "population": 5,
+        "plots": {"farm":2, "house":1}}
+        super().__init__(date)
     
-def updateDay(): # TODO change to datetime with diff 
-    stats["name"] += 1
+    # get village statistic
+    def getStat(self, statistic):
+        return self.stats[statistic]
+
+    # get village plot statistic
+    def getPlot(self, plot):
+        return self.stats["plots"][plot]
+
+    # set village statistic
+    def setStat(self, statistic, value):
+        self.stats[statistic] = value
+        
+    def updateDay(self): # TODO change to datetime with diff 
+        self.stats["name"] += 1
